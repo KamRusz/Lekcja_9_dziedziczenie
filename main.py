c1351b912a68fea97299661a832f9779f888cd8f
Kamil <kamilrusztyn@gmail.com>
2022-02-26 12:49:53 +0100

    ready made program

--- main.py
+++ main.py
@@ -1,64 +1,144 @@
 import csv
-from sys import argv, exit
+from sys import argv
 from os import listdir, path, system, name
+from pathlib import Path
 import json
 import pickle
 
 
-class CommonReader:
-    data = []
+class Reader:
+    flag = "r"
 
     def __init__(self):
-        self.load_file_path = str
-        self.save_file_path = str
-        self.changes = []
-        self.change_flag = True
+        self.load_file_path = argv[1]
 
-    def cls(self):
-        system("cls" if name == "nt" else "clear")
-
-    def testing_parameters(self):
+    def read(self):
         try:
-            self.load_file_path = argv[1]
-            self.save_file_path = argv[2]
-            if path.exists(self.load_file_path):
-                if path.isfile(self.load_file_path):
-                    pass
-                else:
-                    quit(
-                        f"\nścieżka odczytu nie jest plikiem!\n"
-                        "Poniżej lista plików z katalogu"
-                        f" [{path.abspath(self.load_file_path)}]:"
-                        f"\n{listdir(self.load_file_path)}\n"
-                    )
-            else:
-                quit("ścieżka odczytu nie istnieje")
-        except IndexError:
-            exit("nie podano ścieżki")
-        try:
-            self.changes = [x.split(",") for x in argv[3:]]
-        except IndexError:
-            print("nie podano argumentów - plik zapisany bez zmian")
-            self.change_flag = False
+            with open(self.load_file_path, self.flag) as file:
+                return self.read_inner(file)
+        except FileNotFoundError:
+            quit(
+                f"\nścieżka odczytu nie istnieje!\n"
+                "Poniżej lista plików z katalogu"
+                f" [{path.abspath(Path(argv[1]).parent)}]:"
+                f"\n{listdir(path.abspath(Path(argv[1]).parent))}\n"
+            )
 
-    def kind_of_file(self):
-        self.load_ext = self.load_file_path.split(".")[-1]
-        print(self.load_ext)
-        self.save_ext = self.save_file_path.split(".")[-1]
-        print(self.save_ext)
+
+class JsonReader(Reader):
+    def read_inner(self, file):
+        return json.load(file)
+
+
+class CsvReader(Reader):
+    def read_inner(self, file):
+        return list(csv.reader(file, skipinitialspace=True, delimiter=","))
+
+
+class PickleReader(Reader):
+    flag = "rb"
+
+    def read_inner(self, file):
+        return pickle.load(file)
+
+
+class Writer:
+    flag = "w"
+    nl = None
+
+    def __init__(self):
+        self.save_file_path = argv[2]
+
+    def write(self, data):
+        with open(self.save_file_path, self.flag) as file:
+            return self.write_inner(file, data)
+
+
+class JsonWriter(Writer):
+    def write_inner(self, file, data):
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+class CsvWriter(Writer):
+    nl = ""
+
+    def write_inner(self, file, data):
+        csv.writer(file).writerows(data)
+
+
+class PickleWriter(Writer):
+    flag = "wb"
+
+    def write_inner(self, file, data):
+        pickle.dump(data, file)
+
+
+class Manager:
+    read_dict = {
+                ".json": JsonReader,
+                ".csv": CsvReader,
+                ".pickle": PickleReader
+                }
+    write_dict = {
+                ".json": JsonWriter,
+                ".csv": CsvWriter,
+                ".pickle": PickleWriter
+                }
+
+    def __init__(self):
+        try:
+            load_ext = Path(argv[1])
+            if not path.exists(load_ext):
+                quit(
+                    f"\nścieżka odczytu nie istnieje!\n"
+                    "Poniżej lista plików z katalogu"
+                    f" [{path.abspath(load_ext.parent)}]:"
+                    f"\n{listdir(path.abspath(load_ext.parent))}\n"
+                )
+            if not path.isfile(load_ext):
+                quit(
+                    f"\nścieżka odczytu nie jest plikiem!\n"
+                    "Poniżej lista plików z katalogu"
+                    f" [{path.abspath(load_ext.parent)}]:"
+                    f"\n{listdir(load_ext.parent)}\n"
+                )
+        except IndexError:
+            quit("nie podano scieżki odczytu")
+
+        try:
+            save_ext = Path(argv[2])
+            if not save_ext.suffix:
+                quit("\nścieżka zapisu nie jest plikiem!\n")
+        except IndexError:
+            quit("\nnie podano scieżki zapisu")
+
+        self.reader = self.read_dict.get(load_ext.suffix, "no_ext_error")()
+        self.writer = self.write_dict.get(save_ext.suffix, "no_ext_error")()
+
+    def execute(self):
+        self.data = self.reader.read()
+        self.make_change()
+        self.printer()
+        self.writer.write(self.data)
 
     def make_change(self):
-        for change in self.changes:
-            if self.change_flag:
-                if len(self.data) <= int(change[0]) or len(self.data[0]) <= int(
-                    change[1]
-                ):
+        try:
+            self.changes = [x.split(",") for x in argv[3:]]
+            print(self.data)
+            for change in self.changes:
+                if len(self.data) <= int(change[0]) or len(
+                    self.data[0]) <= int(change[1]
+                    ):
                     print(
                         f"\nwspółrzedne [{change[0]},{change[1]}]"
                         " poza zakresem pliku"
                     )
                 else:
-                    self.data[int(change[0])][int(change[1])] = change[2]
+                    self.data[
+                            int(change[0])][int(change[1])
+                             ] = ",".join(change[2:])
+        except IndexError:
+            print("nie podano argumentów - plik zapisany bez zmian")
 
     def printer(self):
         system("cls" if name == "nt" else "clear")
@@ -68,71 +148,6 @@ class CommonReader:
         print("\n")
 
 
-class SpecificReader(CommonReader):
-    def read_csv(self):
-        with open(self.load_file_path, "r", newline="") as self.file:
-            self.reader = csv.reader(
-                self.file, skipinitialspace=True, delimiter=","
-                )
-            CommonReader.data = list(self.reader)
-            return CommonReader.data
-
-    def save_csv(self):
-        with open(self.save_file_path, "w", newline="") as self.file:
-            self.writer = csv.writer(self.file)
-            self.writer.writerows(self.data)
-
-    def read_json(self):
-        with open(self.load_file_path, "r") as self.file:
-            CommonReader.data = json.load(self.file)
-            return CommonReader.data
-
-    def save_json(self):
-        with open(self.save_file_path, "w") as self.file:
-            json.dump(
-                CommonReader.data, self.file, indent=4, ensure_ascii=False
-                )
-
-    def read_pickle(self):
-        with open(self.load_file_path, "rb") as self.file:
-            CommonReader.data = pickle.load(self.file)
-            return CommonReader.data
-
-    def save_pickle(self):
-        with open(self.save_file_path, "wb") as self.file:
-            pickle.dump(CommonReader.data, self.file, pickle.HIGHEST_PROTOCOL)
-
-def main():
-    plik = SpecificReader()
-    plik.testing_parameters()
-    plik.kind_of_file()
-
-    if plik.load_ext == "csv":
-        plik.read_csv()
-    elif plik.load_ext == "json":
-        plik.read_json()
-    elif plik.load_ext == "pickle":
-        plik.read_pickle()
-
-    # dispatcher2 = {
-    # "load":{
-    #       "csv":plik.read_csv, "json":SpecificReader.read_json,
-    #       "pickle":SpecificReader.read_json},
-    # "save":{"csv":SpecificReader.save_csv, "json":SpecificReader.save_json,
-    #        "pickle":SpecificReader.save_json}
-    # }
-    #dispatcher2["load"]["csv"]()
- 
-    plik.make_change()
-    plik.printer()
-
-    if plik.save_ext == "csv":
-        plik.save_csv()
-    elif plik.save_ext == "json":
-        plik.save_json()
-    else:
-        plik.save_pickle()
-
-
 if __name__ == "__main__":
-    main()
+    obj = Manager()
+    obj.execute()
